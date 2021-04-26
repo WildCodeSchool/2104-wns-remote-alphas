fef2ab12167d3ae01444ea0b400cbed608545591
Theodore <theodore.lefrancois@onrewind.com>
2021-04-26 15:55:30 +0200

    test configs

--- packages/front/src/App.tsx
+++ packages/front/src/App.tsx
@@ -1,9 +1,9 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import logo from './logo.svg'
+import './App.css'
 
 function App() {
-	console.log('epbeirb');
+	console.log('epbeirb')
 	return (
 		<div className="App">
 			<header className="App-header">
@@ -21,7 +21,7 @@ function App() {
 				</a>
 			</header>
 		</div>
-	);
+	)
 }
 
-export default App;
+export default App
