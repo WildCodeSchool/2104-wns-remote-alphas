f0b6c73df72f564fdd58a923fb674ac3e7c1eb5a
Theodore <theodore.lefrancois@onrewind.com>
2021-04-29 15:48:54 +0200

    make the connection between front and back, and get the courses

--- front/src/Router.tsx
+++ front/src/Router.tsx
@@ -1,8 +1,20 @@
+import { gql, useQuery } from '@apollo/client';
 import React from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import Layout from './components/Layout.styled';
 
 function Router(): JSX.Element {
+	const { data } = useQuery(gql`
+		query {
+			getCourses {
+				description
+				technos
+				courseName
+				image_url
+			}
+		}
+	`);
+	if (data) console.log(data);
 	return (
 		<BrowserRouter>
 			<Layout>
--- front/src/index.tsx
+++ front/src/index.tsx
@@ -1,28 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { ApolloClient, InMemoryCache, gql } from '@apollo/client';
+import { ApolloClient, InMemoryCache } from '@apollo/client';
 import { ApolloProvider } from '@apollo/client/react';
 import './index.css';
 import Router from './Router';
 
-// todo: set a constant API uri in a .env file
 const client = new ApolloClient({
 	uri: process.env.REACT_APP_API_DEV,
-	cache: new InMemoryCache()
+	cache: new InMemoryCache(),
 });
-
-// todo: query API
-client
-	.query({
-		query: gql`
-      query GetRates {
-        rates(currency: "USD") {
-          currency
-        }
-      }
-    `
-	})
-	.then((result) => console.log(result));
 
 ReactDOM.render(
 	<React.StrictMode>
