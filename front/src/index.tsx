cc7c694baa2f1e00ba010bdbce1b2f6b0c3f5ff4
Elya29 <elya29870@gmail.com>
2021-04-29 10:47:22 +0200

    set up apollo client

--- front/src/index.tsx
+++ front/src/index.tsx
@@ -1,11 +1,34 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { ApolloClient, InMemoryCache, gql } from '@apollo/client';
+import { ApolloProvider } from '@apollo/client/react';
 import './index.css';
 import Router from './Router';
 
+// todo: set a constant API uri in a .env file
+const client = new ApolloClient({
+	uri: 'https://48p1r2roz4.sse.codesandbox.io',
+	cache: new InMemoryCache()
+});
+
+// todo: query API
+client
+	.query({
+		query: gql`
+      query GetRates {
+        rates(currency: "USD") {
+          currency
+        }
+      }
+    `
+	})
+	.then((result) => console.log(result));
+
 ReactDOM.render(
 	<React.StrictMode>
-		<Router />
+		<ApolloProvider client={client}>
+			<Router />
+		</ApolloProvider>
 	</React.StrictMode>,
 	document.getElementById('root')
 );
