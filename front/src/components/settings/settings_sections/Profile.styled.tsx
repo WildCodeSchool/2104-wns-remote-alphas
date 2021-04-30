5f8e03f35388f67c143c97de9d5c6708925e1cd7
Elya29 <elya29870@gmail.com>
2021-04-30 13:43:14 +0200

    wip profile content

--- front/src/components/settings/settings_sections/Profile.styled.tsx
+++ front/src/components/settings/settings_sections/Profile.styled.tsx
@@ -1,16 +1,41 @@
 import React from 'react';
 import styled from 'styled-components';
-import FormField from '../../FormField.styled';
+import Field from '../../Field.styled';
 
 /**
  * NavMenu displays the setting cards' navigation menu, with links and icons
  */
 const Form = styled.form`
+    display: grid;
+    grid-template-columns: 50% 50%;
+    width: 100%;
+    margin: 1em;
+    input:focus + label{
+        color: #FE7F2D;
+        font-weight: bold;
+    }
+    label {
+        font-weight: 500;
+    }
+`;
+
+const Column = styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: baseline;
+    margin: 1em;
 `;
 
 const Profile = (): JSX.Element => (
     <Form>
-        <FormField type="text" label="name" />
+        <Column>
+            <Field type="text" label="Name" />
+            <Field type="email" label="Email" />
+        </Column>
+        <Column>
+            <Field type="text" label="First Name" />
+            <Field type="text" label="Location" />
+        </Column>
     </Form>
 );
 
