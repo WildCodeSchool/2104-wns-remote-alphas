abdf8995caa84bff79b19fd9357320dcad7cc7de
anthony <anthony.verges40@gmail.com>
2021-05-28 17:25:44 +0200

    modify type courseInput

--- back/src/Resolvers/types/CourseInput.ts
+++ back/src/Resolvers/types/CourseInput.ts
@@ -3,15 +3,15 @@ import { InputType, Field } from "type-graphql";
 
 @InputType()
 export class CourseInput implements Partial<Course> {
-  @Field((type) => String)
+  @Field((type) => String, { nullable: true })
   courseName!: string;
 
-  @Field((type) => String)
+  @Field((type) => String, { nullable: true })
   description!: string;
 
-  @Field((type) => String)
+  @Field((type) => String, { nullable: true })
   image_url!: string;
 
-  @Field((type) => [String])
+  @Field((type) => [String], { nullable: true })
   technos!: string[];
 }
