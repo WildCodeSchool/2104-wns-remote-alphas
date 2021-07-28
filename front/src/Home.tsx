7fee193872f56dd7d1813b20f304c08e993d22e9
ho823 <ameline.houis@hotmail.fr>
2021-07-28 17:19:40 +0200

    add splice on array to display only last 3 element (newer)

--- front/src/Home.tsx
+++ front/src/Home.tsx
@@ -41,11 +41,9 @@ function Home(): JSX.Element {
   <ThemeProvider theme={darkTheme}>
     <AppContent>
       <CardContainer>
-        {data.getCourses.map((course: CourseType) => (
+        {data.getCourses.slice(-3).map((course: CourseType) => (
           <CardCoursesSecondary title={course.courseName} image={course.image_url} imageDescription="image video" course={course.technos[0]} />
           ))}
-          <CardCourses title="GraphQL API" image="assets/images/graphql.png" imageDescription="image video" course="front-end | GraphQL" />
-          <CardCoursesSecondary title="Typescript" image="/assets/images/ts.png" imageDescription="image video" course="front-end | Typescript" />
       </CardContainer>
     </AppContent>
   </ThemeProvider>
