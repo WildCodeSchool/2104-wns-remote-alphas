2aa1033d254276adba5eca12b31a86d0d36b506c
Theodore <theodore.lefrancois@onrewind.com>
2021-07-30 12:11:59 +0200

    add query me to get user datas

--- back/src/Resolvers/UserResolver.ts
+++ back/src/Resolvers/UserResolver.ts
@@ -1,8 +1,8 @@
-import { Resolver, Arg, Mutation, Query } from "type-graphql";
+import { Resolver, Arg, Mutation, Query, Ctx } from "type-graphql";
 import { User, UserModel } from "../Models/User";
 import { UserInput } from "./types/UserInput";
 import bcrypt from "bcrypt";
-import { ApolloError } from "apollo-server";
+import { ApolloError, AuthenticationError } from "apollo-server";
 
 @Resolver((of) => User)
 export class UserResolver {
@@ -10,6 +10,25 @@ export class UserResolver {
   async getUsers(): Promise<User[]> {
     const users = await UserModel.find();
     return users;
+  }
+  @Query((returns) => User)
+  async me(
+    @Ctx() { authenticatedUserEmail }: { authenticatedUserEmail: string }
+  ) {
+    console.log(authenticatedUserEmail);
+
+    if (authenticatedUserEmail) {
+      const users = await UserModel.find();
+      const me = users.find((user) => user.email === authenticatedUserEmail);
+      if (me) {
+        me.password = "";
+        return me;
+      } else {
+        throw new ApolloError("User not found");
+      }
+    } else {
+      throw new AuthenticationError("Not connected");
+    }
   }
 
   @Mutation((returns) => User)
