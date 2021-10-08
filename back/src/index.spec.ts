1c2a54eb00c7a212f6fd7e5f86e3c3e8589bbb1a
anthony <anthony.verges40@gmail.com>
2021-10-08 10:07:07 +0200

    hot fix ci

--- back/src/index.spec.ts
+++ back/src/index.spec.ts
@@ -11,6 +11,12 @@ import {
   GET_USERS,
 } from "./Test";
 const { createTestClient } = require("apollo-server-testing");
+import jwt from "jsonwebtoken";
+
+if (!process.env.SECRET_KEY) {
+  throw new Error("environment variable SECRET_KEY is missing");
+}
+const jwtKey = process.env.SECRET_KEY;
 
 describe("Tests for the back", () => {
   let apollo: ApolloServer | null = null;
@@ -30,7 +36,24 @@ describe("Tests for the back", () => {
       const schema = await buildSchema({
         resolvers: [CourseResolver, UserResolver],
       });
-      const server: ApolloServer = new ApolloServer({ schema });
+      const server: ApolloServer = new ApolloServer({
+        schema,
+        context: ({ req }) => {
+          if (req) {
+            const token = req.headers.authorization;
+            if (token) {
+              try {
+                const payload = jwt.verify(token, jwtKey);
+                if (typeof payload !== "string") {
+                  return { authenticatedUserEmail: payload.userEmail };
+                }
+              } catch (err) {
+                console.log(err);
+              }
+            }
+          }
+        },
+      });
       return server;
     };
     apollo = await initApolloServer();
@@ -48,6 +71,10 @@ describe("Tests for the back", () => {
     const { mutate } = createTestClient(apollo);
     const res = await mutate({
       mutation: INSERT_NEW_COURSE,
+      headers: {
+        authenticatedUserEmail:
+          "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyRW1haWwiOiJ2ZXJnZXNfYW50aG9ueUBob3RtYWlsLmZyIiwiaWF0IjoxNjMzNjc5OTQ1fQ.-U27XzyH4eYVDl-tjLyINjn7P99myIIoK5u9FLFM-zQ",
+      },
     });
     expect(res.data?.addCourse.courseName).toEqual("Test");
     expect(res.data?.addCourse.image_url).toEqual(
@@ -61,6 +88,10 @@ describe("Tests for the back", () => {
     const { query } = createTestClient(apollo);
     const res = await query({
       query: GET_COURSES,
+      headers: {
+        authenticatedUserEmail:
+          "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyRW1haWwiOiJ2ZXJnZXNfYW50aG9ueUBob3RtYWlsLmZyIiwiaWF0IjoxNjMzNjc5OTQ1fQ.-U27XzyH4eYVDl-tjLyINjn7P99myIIoK5u9FLFM-zQ",
+      },
     });
     expect(res.data.getCourses.length).toEqual(1);
   });
