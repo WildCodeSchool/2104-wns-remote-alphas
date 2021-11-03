240c53b51866a19a0f0f13561f7c5c4e697f8626
ho823 <ameline.houis@hotmail.fr>
2021-11-03 14:22:39 +0100

    resolve conflict

--- front/src/index.tsx
+++ front/src/index.tsx
@@ -1,12 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-<<<<<<< HEAD
-import { ApolloProvider } from '@apollo/client';
-import client from './utils/apolloclient';
-=======
-
->>>>>>> 5bd994ec2e860bd37dcf3549f5508ecdfb4527d8
-import './index.css';
 import Router from './Router';
 
 ReactDOM.render(
