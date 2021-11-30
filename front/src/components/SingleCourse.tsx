71c5f8793db2edc5aa66250afd71383eb115e2d3
ho823 <ameline.houis@hotmail.fr>
2021-11-30 14:00:54 +0100

    css single course

--- front/src/components/SingleCourse.tsx
+++ front/src/components/SingleCourse.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import { useHistory } from 'react-router-dom';
 import styled from 'styled-components';
+import { useQuery, gql } from '@apollo/client';
+import ScrollToTop from './ScrollToTop';
 
 const Container = styled.div`
     display: flex;
@@ -8,7 +10,29 @@ const Container = styled.div`
     align-items: center;
     flex-direction: column;
     padding: 141px;
+`;
 
+const BackButton = styled.button`
+    background-color: transparent;
+    border: none;
+    color: white;
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    width: 125px;
+    align-self: flex-start;
+    margin: 0 0 2% 12%;
+`;
+
+const ArrowContent = styled.div`
+    width: 32px;
+`;
+
+const Arrow = styled.img`
+    width: 100%;
+`;
+
+const ButtonText = styled.p`
 `;
 
 const Course = styled.div`
@@ -17,7 +41,7 @@ const Course = styled.div`
     border-radius: 30px;
     background-color: #ECEFF1;
     padding: 65px 121px 65px 121px;
-    border: solid red 1px;
+    box-shadow: 10px 12px 16px -3px rgba(0,0,0,0.75);
 `;
 
 const TextOnHead = styled.div`
@@ -25,7 +49,10 @@ const TextOnHead = styled.div`
     justify-content: space-between;
 `;
 
-const linkOfVideo = styled.p`
+const LinkOfVideo = styled.p`
+    color: #FE7F2D;
+`;
+const LinkReference = styled.td`
     color: #FE7F2D;
 `;
 
@@ -37,13 +64,34 @@ const PandaImage = styled.img`
     width: 100%;
 `;
 
+export const GET_COURSES_BY_ID_QUERY = gql`
+	query {
+		getCourseById {
+			_id
+			description
+			technos
+			courseName
+			image_url
+		}
+	}
+`;
+
 function SingleCourse(): JSX.Element {
     const history = useHistory();
+    const { loading, error, data } = useQuery(GET_COURSES_BY_ID_QUERY);
+
 	return (
         <Container>
-            <div>
-                <button type="button" onClick={() => history.goBack()}>Go back</button>
-            </div>
+{/*              {data.getCourseById.map((course: any) => (
+                // eslint-disable-next-line no-underscore-dangle
+                <p key={course._id}>{course.name}</p>
+            ))} */}
+                <BackButton type="button" onClick={() => history.goBack()}>
+                    <ArrowContent>
+                        <Arrow src="/assets/icons/018-arrowhead-pointing-to-the-right-1.svg" alt="arrow icon" />
+                    </ArrowContent>
+                    <ButtonText>Go back</ButtonText>
+                </BackButton>
             <Course>
                 <TextOnHead>
                     <p>GraphQL Basics</p>
@@ -51,7 +99,7 @@ function SingleCourse(): JSX.Element {
                 </TextOnHead>
                 <div>
                     <img src="https://via.placeholder.com/600x350" alt="" />
-                    <p>Lien vidéo - Thomas</p>
+                    <LinkOfVideo>Lien vidéo - Thomas</LinkOfVideo>
                 </div>
                 <section>
                     <h4>fknjdbjkcbdkbchbdc</h4>
@@ -80,13 +128,13 @@ function SingleCourse(): JSX.Element {
                     <h3>Going further...</h3>
                     <table>
                         <tr>
-                            <td>Reference Link</td>
-                            <td>Reference Link</td>
+                            <LinkReference>Reference Link</LinkReference>
+                            <LinkReference>Reference Link</LinkReference>
                         </tr>
                         <tr>
-                            <td>Reference Link</td>
-                            <td>Reference Link</td>
-                            <td>Reference Link</td>
+                            <LinkReference>Reference Link</LinkReference>
+                            <LinkReference>Reference Link</LinkReference>
+                            <LinkReference>Reference Link</LinkReference>
                         </tr>
                     </table>
                 </div>
@@ -94,6 +142,7 @@ function SingleCourse(): JSX.Element {
                     <PandaImage src="/assets/images/ninja-goodJob.png" alt="ninja panda says good job" />
                 </NinjaPanda>
             </Course>
+            <ScrollToTop />
         </Container>
 
 	);
