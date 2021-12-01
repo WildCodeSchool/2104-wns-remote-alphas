b05d4bbf53077ff174b30c5655e3ea282a4b16dc
Elya29 <elya29870@gmail.com>
2021-12-02 00:21:20 +0100

    add comments to the custom hook

--- front/src/utils/useWindowSize.ts
+++ front/src/utils/useWindowSize.ts
@@ -1,5 +1,21 @@
 import React from 'react';
 
+/** Defines a custom hook for building a responive app depending on the
+ * current window's size.
+ *
+ * Sample use of display only when window is larger than 600px :
+ * ```
+ *  import useWindowSize from '../../../utils/useWindowSize';
+ *  const { width } = useWindowSize();
+ *
+ *  return(
+ *
+ *      {width > 600 && (
+            <text>Colors</text>
+        )}
+ *  )
+ * ```
+*/
 export default function useWindowSize(): {
     [key: string]: number
 } {
@@ -8,6 +24,7 @@ export default function useWindowSize(): {
         height: window.innerHeight,
     });
 
+    /// update window size on resizing
     function changeWindowSize() {
         setWindowSize({ width: window.innerWidth, height: window.innerHeight });
     }
@@ -16,6 +33,7 @@ export default function useWindowSize(): {
         window.addEventListener('resize', changeWindowSize);
 
         return () => {
+            /// clean the listeners when component unmounts
             window.removeEventListener('resize', changeWindowSize);
         };
     }, []);
