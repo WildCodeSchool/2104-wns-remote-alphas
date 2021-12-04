aa2de5844265b912431eb194d07339a7d8813c94
Elya29 <elya29870@gmail.com>
2021-12-04 01:59:06 +0100

    fix settings nav height overflow

--- front/src/components/settings/components/SettingsContainer.styled.tsx
+++ front/src/components/settings/components/SettingsContainer.styled.tsx
@@ -6,7 +6,6 @@ import styled from 'styled-components';
 const Container = styled.div`
     display: grid;
     flex-direction: column;
-    margin: ${(props) => props.theme.margin.generic.large};
     flex-wrap: wrap;
     justify-content: stretch;
     width: 60%;
