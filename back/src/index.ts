2b8c1db5d17ef6a3ab37e1f550f58f64e3a62271
ho823 <ameline.houis@hotmail.fr>
2022-02-21 16:02:08 +0100

    try to put function for push token in back but doesnt work

--- back/src/index.ts
+++ back/src/index.ts
@@ -10,6 +10,73 @@ import {
   MessageResolver,
 } from "./Resolvers";
 import jwt, { JwtPayload } from "jsonwebtoken";
+import { Expo } from 'expo-server-sdk';
+
+let expo = new Expo({ accessToken: process.env.EXPO_ACCESS_TOKEN });
+
+let messages = [];
+for (let pushToken of somePushTokens) {
+
+  if (!Expo.isExpoPushToken(pushToken)) {
+    console.error(`Push token ${pushToken} is not a valid Expo push token`);
+    continue;
+  }
+
+  messages.push({
+    to: pushToken,
+    sound: 'default',
+    body: 'This is a test notification',
+    data: { withSome: 'data' },
+  })
+}
+
+let chunks = expo.chunkPushNotifications(messages);
+let tickets: any = [];
+(async () => {
+
+  for (let chunk of chunks) {
+    try {
+      let ticketChunk = await expo.sendPushNotificationsAsync(chunk);
+      console.log(ticketChunk);
+      tickets.push(...ticketChunk);
+    } catch (error) {
+      console.error(error);
+    }
+  }
+})();
+
+let receiptIds = [];
+for (let ticket of tickets) {
+  if (ticket.id) {
+    receiptIds.push(ticket.id);
+  }
+}
+
+let receiptIdChunks = expo.chunkPushNotificationReceiptIds(receiptIds);
+(async () => {
+
+  for (let chunk of receiptIdChunks) {
+    try {
+      let receipts = await expo.getPushNotificationReceiptsAsync(chunk);
+      console.log(receipts);
+      for (let receiptId in receipts) {
+        let { status, message, details } = receipts[receiptId];
+        if (status === 'ok') {
+          continue;
+        } else if (status === 'error') {
+          console.error(
+            `There was an error sending a notification: ${message}`
+          );
+          if (details && details.error) {
+            console.error(`The error code is ${details.error}`);
+          }
+        }
+      }
+    } catch (error) {
+      console.error(error);
+    }
+  }
+})();
 
 if (!process.env.MONGODB) {
   throw new Error("environment variable MONGODB is missing");
