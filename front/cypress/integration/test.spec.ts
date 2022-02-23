a8019bcdf9a06c341fb6c32b034b204129013c6f
ho823 <ameline.houis@hotmail.fr>
2022-02-23 10:43:05 +0100

    begin to add a end to end test with cypress

--- front/cypress/integration/test.spec.ts
+++ front/cypress/integration/test.spec.ts
@@ -3,5 +3,8 @@ describe('My First Test', () => {
       cy.visit('/');
       cy.get('a[href*="/signin"]')
       .click()
+      cy.get('input[name*="email"]').type('ameline@gmail.com')
+      cy.get('input[name*="password"]').type('password')
+      cy.get('form').submit()
     })
   })
