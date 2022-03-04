3ab9c9884d58eb89d28da058c50a263e0f3b8790
Alex Palma <alex.palma@arke.fr>
2022-03-04 12:53:03 +0100

    add descriptions to user fields in user model

--- back/src/Models/User.ts
+++ back/src/Models/User.ts
@@ -8,37 +8,38 @@ import { ObjectId } from "mongodb";
 import { Field, ID, ObjectType } from "type-graphql";
 import { Settings } from "./Settings";
 
-@ObjectType({description:'This is a user -and a test description, by the way-'})
+@ObjectType({description:'A user could be a student, a teacher or an administator'})
 @modelOptions({ options: { allowMixed: Severity.ALLOW } })
 export class User {
-  @Field((type) => ID)
+  @Field((type) => ID, {description:'The user unique identifier'})
   readonly _id!: ObjectId;
 
-  @Field((type) => String)
+  @Field((type) => String, {description:'The user family name'})
   @prop({ required: true })
   public name!: string;
 
-  @Field((type) => String)
+  @Field((type) => String, {description:'The user first name'})
   @prop({ required: true })
   public firstName!: string;
 
-  @Field((type) => String)
+  @Field((type) => String, {description:'The user email'})
   @prop({ required: true })
   public email!: string;
 
-  @Field((type) => String)
+  // FIXME: Hash passwords !!! 😱😱😱
+  @Field((type) => String, {description:'The user password'})
   @prop({ required: true })
   public password!: string;
 
-  @Field((type) => String)
+  @Field((type) => String, {description:'The user role defines his authorizations'})
   @prop({ required: true })
   public role!: "student" | "teacher" | "admin";
 
-  @Field((type) => String, { nullable: true })
+  @Field((type) => String, { nullable: true, description:'The user optionnal location' })
   @prop({ required: false })
   public location?: string;
 
-  @Field((type) => Settings, { nullable: true })
+  @Field((type) => Settings, { nullable: true, description: 'The user personnal settings and theming data' })
   @prop({ required: false })
   public settings?: Settings;
 }
