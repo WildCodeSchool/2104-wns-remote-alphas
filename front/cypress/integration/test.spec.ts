126d8197c98f7fd556c81a68adb40aee0a7d18c2
ho823 <ameline.houis@hotmail.fr>
2022-04-18 11:27:33 +0200

    trying to make something with cypress

--- front/cypress/integration/test.spec.ts
+++ front/cypress/integration/test.spec.ts
@@ -1,11 +1,9 @@
 describe('My First Test', () => {
     it('It Works', () => {
       cy.visit('/');
-      cy.get('a[href*="/signin"]')
-      .click()
+      cy.get('button').contains('Log in').click()
       cy.get('input[name*="email"]').type('ameline@gmail.com')
       cy.get('input[name*="password"]').type('password')
-      //cy.get('form').submit()
-      cy.get('button[value*="Envoyer"]').click()
+      cy.get('button[type*="submit"]').click()
     })
   })
